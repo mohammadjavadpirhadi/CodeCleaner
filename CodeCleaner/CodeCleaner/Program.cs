6de3d8c87e2659a0356a2b421cfac72a5dd4d093
Mohammad Javad Pirhadi <mohammadjavad.pirhadi@gmail.com>
2019-11-15 11:26:49 +0330

    Initialize project

---
+++ CodeCleaner/CodeCleaner/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeCleaner
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Scanner scanner = new Scanner("../../assets/Test.cs");
+            Parser parser = new Parser(scanner);
+            parser.Parse();
+            Console.WriteLine("Number of errors detected: {0}", parser.errors.count);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ CodeCleaner/CodeCleaner/assets/Test.cs
@@ -0,0 +1,32 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P1Test
+{
+    class progam
+    {
+        static void Main(string[] args)
+        {
+            string[] arr = new string[10];
+            int i = 0;
+            for (int j = 0; j < arr.Length; i++)
+            {
+                arr[j] = get(i+1)
+                i++;
+            }
+            while (i>=0)
+            {
+                Console.WriteLine(arr[j]);
+                i--;
+            }
+        }
+
+        int get(int i)
+        {
+            return i.ToString();
+        }
+    }
+}
